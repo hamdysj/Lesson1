e18811d0c57d5b99d7205fb79a8bd6fe6a8b8f8a
Hamdallah Sulaimon <hamdysj@gmail.com>
2021-12-28 16:45:11 +0100

    Asynchronous Programming
    Working with Callback functions in NodeJS

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
+
+
 const fs = require('fs');
+/*
+//Blocking & Synchronous Way: Executes LineByLine
 const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
 console.log(textIn);
 
 const textOut = `This is what we know about avocado ${textIn}. \n Created on ${Date.now}`;
 fs.writeFileSync('./txt/output.txt', textOut);
 console.log("File Written");
+*/
+
+
+//Non-Blocking & Asynchronous Way: Uses Callback Functions
+fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+    fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+        console.log(data2);
+        fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
+            console.log(data3);
+
+            fs.writeFile('./txt/finals.txt', `${data2}\n${data3}`, (err, data3) => {
+                console.log('File written successfully');       
+            })
+        });
+    });
+});
+console.log("Will execute first");
+
